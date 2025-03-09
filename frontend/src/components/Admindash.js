87568e46faabf4e93e7e14e6411d9245fb37654d
Abinnbabu <133137438+Abinnbabu@users.noreply.github.com>
2025-03-09 18:21:13 +0530

    Updated Admindash.js and removed Templatemangement Button from the code. Also updated the stylesa.css for it
    U

--- frontend/src/components/Admindash.js
+++ frontend/src/components/Admindash.js
@@ -21,25 +21,25 @@ const AdminDashboard = () => {
         <h1>Career Catalyst</h1>
         <h1>Admin Dashboard</h1>
         <Link to="/index">
-        <button onClick={logout} className="button-secondary">
-          User Dashboard/logout
-        </button>
+          <button onClick={logout} className="button-secondary">
+            User Dashboard/logout
+          </button>
         </Link>
-
       </header>
       <div className="admin-content">
         <section className="admin-section">
           <Link to="/AtsScoreControl">
             <button className="button">ATS Score Control</button>
           </Link>
-          
           <ul id="ats-log-list" className="admin-list"></ul>
-        </section>
-        <section className="admin-section">
+
+          {/* <section className="admin-section">
           <button onClick={TemplateManagement} className="button">
             Template Management
           </button>
           <ul id="template-list" className="admin-list"></ul>
+        </section>
+        */}
         </section>
       </div>
     </div>
