b344fa8c5d549e78ef5bbb73bc58ee5088d9e4fb
Aaron Peter Viju <aaron.cse22@mbits.ac.in>
2025-03-12 11:27:51 +0530

    deprecated options replaced with alternatives

--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,13 @@ app.get('/', (req, res) => {
 
 const mongoURI = 'mongodb://127.0.0.1:27017/resume-builder';
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(mongoURI)
+  .then(() => {
     console.log('Connected to MongoDB');
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.error('Error connecting to MongoDB:', error.message);
-});
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
