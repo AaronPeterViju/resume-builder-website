1553df7c5f68b263d6f2e477abf07ad74eaca425
Abinnbabu <133137438+Abinnbabu@users.noreply.github.com>
2025-03-15 23:38:17 +0530

    Update server.js for Super Admin

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const resumeRoutes = require('./routes/resume');
 const atsRoutes = require('./routes/ats');
+const userRoutes = require('./routes/userRoutes'); ; // Added this for Admin Management
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/resume', resumeRoutes);
 app.use('/api/ats', atsRoutes);
+app.use('/api/users', userRoutes); // Admin Management Route Added
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Resume Builder API');
