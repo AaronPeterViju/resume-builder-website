5b36d84b7cfd86411ccffae000394c11dcc1bb15
Abinnbabu <133137438+Abinnbabu@users.noreply.github.com>
2025-03-23 12:14:21 +0530

    Update User.js for normal admin redirection

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,11 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: {
-        type: String,
-        enum: ['user', 'admin', 'superadmin'],
-        default: 'user' // Default role is 'user'
-    }
+    role: { type: String, default: 'user' } // Default role is 'user'
 });
 
 const User = mongoose.model('User', UserSchema);
